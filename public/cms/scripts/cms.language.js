a6c7d5de81b729740bdb83b4f5842e12eda47495
Iram Gutiérrez <iramgutzglez@gmail.com>
2015-12-30 00:13:15 -0600

    lang

--- public/cms/scripts/cms.language.js
+++ public/cms/scripts/cms.language.js
@@ -1,70 +1,89 @@
 'use strict'
 
 angular.module('cms.language' , [])
-    .value('language' , {
-        es : {
-            company : 'CMS',
-            posts : 'entradas',
-            categories : 'categorías',
-            logout : 'salir',
-            name : 'nombre',
-            slug : 'slug',
-            available : 'disponible',
-            excerpt : 'extracto',
-            content : 'contenido',
-            category : 'categoria',
-            created_date : 'fecha de creación',
-            updated_date : 'última modificación',
-            custom_fields : 'campos personalizados',
-            field : 'campo',
-            value : 'valor',
-            post_saved : 'entrada guardada',
-            post_updated : 'entrada actualizada',
-            post_deleted : 'entrada eliminada',
-            category_saved : 'categoría guardada',
-            category_updated : 'categoría actualizada',
-            category_deleted : 'categoría eliminada',
-            invalid_format_image : 'formato de imagen invalido',
-            image : 'imagen',
-            username : 'nombre de usuario',
-            password : 'contraseña',
-            unauthorized : 'no autorizado',
-            server_error : 'error de servidor',
-            the_name_field_is_required : 'el campo nombre es requerido',
-            the_category_id_field_is_required : 'el campo categoría es requerido',
-            the_excerpt_field_is_required : 'el campo extracto es requerido',
-            the_content_field_is_required : 'el campo contenido es requerido',
-            the_name_has_already_been_taken : 'el campo nombre tiene que ser único'
-        },
-        en: {}
-    })
-    .filter('language', ['config', 'language', function (config, language) {
+    .factory('language' , ['$http' ,'$q' , 'config', function($http , $q , config){
 
-        return function (input, capitalize) {
+        var langs = {
+            es : {
+                company : 'CMS',
+                posts : 'entradas',
+                categories : 'categorias',
+                logout : 'salir',
+                name : 'nombre',
+                slug : 'slug',
+                available : 'disponible',
+                excerpt : 'extracto',
+                content : 'contenido',
+                category : 'categoría',
+                created_date : 'fecha de creación',
+                updated_date : 'última modificación',
+                custom_fields : 'campos personalizados',
+                field : 'campo',
+                value : 'valor',
+                post_saved : 'entrada guardada',
+                post_updated : 'entrada actualizada',
+                post_deleted : 'entrada eliminada',
+                category_saved : 'categoría guardada',
+                category_updated : 'categoría actualizada',
+                category_deleted : 'categoría eliminada',
+                invalid_format_image : 'formato de imagen invalido',
+                image : 'imagen',
+                username : 'nombre de usuario',
+                password : 'contraseña',
+                unauthorized : 'no autorizado',
+                server_error : 'error de servidor',
+                the_name_field_is_required : 'el campo nombre es requerido',
+                the_category_id_field_is_required : 'el campo categoría es requerido',
+                the_excerpt_field_is_required : 'el campo extracto es requerido',
+                the_content_field_is_required : 'el campo contenido es requerido',
+                the_name_has_already_been_taken : 'el campo nombre tiene que ser único'
+            },
+            en: {}
+        }
 
-            var input = input || '';
-
-            var capitalize = capitalize || false;
+        function get(input , capitalize) {
 
             if (config.hasOwnProperty('locale')) {
+
                 var locale = config.locale;
 
-                if (language.hasOwnProperty(locale)) {
-                    var lang = language[locale]
+                if(langs.hasOwnProperty(locale))
+                {
+
+                    var lang= langs[locale];
 
                     if (lang.hasOwnProperty(input)) {
 
                         input = lang[input]
                     }
+
+                    if (capitalize) {
+                        return input.toLowerCase().substring(0, 1).toUpperCase() + input.substring(1);
+                    }
+
+                    return input;
+
                 }
             }
 
-            if (capitalize) {
-                return input.toLowerCase().substring(0, 1).toUpperCase() + input.substring(1);
-            }
-
             return input;
-
         }
 
+        return {
+            get : get
+        }
+        
     }])
+    .filter('language', function (config, language , $q) {
+
+            return function(input, capitalize) {
+
+                var input = input || '';
+
+                var capitalize = capitalize || false;
+
+                return language.get(input , capitalize);
+
+            }
+
+    })
